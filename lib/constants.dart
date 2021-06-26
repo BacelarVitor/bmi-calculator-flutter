677912e8b0fce0279e21d54bf927fba5e3b80bb4
vitor.bacelar <v.bacelar@intcom.com.br>
2021-06-26 15:05:54 -0300

    finish project

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 const kActiveCardColour = Color(0xFF1D1E33);
 const kInactiveCardColour = Color(0xFF111328);
 const kBottomButtonColour = Color(0xFFEB1555);
-const kBottomButtonHeight = 80.0;
+const kBottomButtonHeight = 60.0;
 const kLabelTextStyle = TextStyle(color: Color(0xFF8D8E98), fontSize: 18.0);
 const kNumberTextStyle = TextStyle(fontSize: 50.0, fontWeight: FontWeight.w900);
+const kLargeButtonTextStyle =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0);
+const kAppTitle = Text('BMI CALCULATOR');
+const kTitleTextStyle = TextStyle(fontWeight: FontWeight.bold, fontSize: 50.0);
+const kResultTextStyle = TextStyle(
+    color: Color(0xFF24D876), fontWeight: FontWeight.bold, fontSize: 22.0);
+const kBMITextStyle = TextStyle(fontWeight: FontWeight.bold, fontSize: 100.0);
+const kBodyTextStyle = TextStyle(fontSize: 22.0);
